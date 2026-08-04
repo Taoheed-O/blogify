659d6b85b2b5bd928d1e90678024871ddd513c85
Taoheed-O <oyeniyiemperor@gmail.com>
2026-08-04 08:19:37 +0100

    Added featured posts...

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blogs.models import Category, Blog
 
 
 
 # Homepage...
 def home(request):
-    return render(request, 'home.html')
+    # All categories
+    categories = Category.objects.all()
+    # Featured posts
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
+
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts
+    }
+    return render(request, 'home.html', context=context)
