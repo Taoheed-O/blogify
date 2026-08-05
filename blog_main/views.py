775a524f810d584722ee4c01aa1b80d5cb955bd0
Taoheed-O <oyeniyiemperor@gmail.com>
2026-08-05 15:49:33 +0100

    code refactoring

--- blog_main/views.py
+++ blog_main/views.py
@@ -10,11 +10,14 @@ def home(request):
     categories = Category.objects.all()
     # Featured posts
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    # Not Featured posts
+    not_featured_posts = Blog.objects.filter(is_featured=False, status='Published').order_by('-updated_at')
     # Posts
     posts = Blog.objects.filter(is_featured=False, status='Published')
 
     context = {
         'categories': categories,
-        'featured_posts': featured_posts
+        'featured_posts': featured_posts,
+        'not_featured_posts': not_featured_posts,
     }
     return render(request, 'home.html', context=context)
