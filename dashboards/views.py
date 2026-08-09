922a702115c30900f4f21c299dc0054002be173f
Taoheed-O <oyeniyiemperor@gmail.com>
2026-08-09 18:08:29 +0100

    Creating dashboards...

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from blogs.models import Blog, Category
 
 # Create your views here.
+
+# Dashboard
+def dashboard(request):
+    blog_count = Blog.objects.all().count()
+    category_count = Category.objects.all().count()
+    featured_count = Blog.objects.filter(is_featured=True).count()
+    published_count = Blog.objects.filter(status='Published').count()
+    drafted_count = Blog.objects.filter(status='Draft').count()
+
+    context = {
+        "blog_count": blog_count,
+        "category_count": category_count,
+        "featured_count": featured_count,
+        "published_count": published_count,
+        "drafted_count": drafted_count
+    }
+    return render(request,'dashboards/dashboard.html', context=context)
