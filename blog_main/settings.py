c0d716814c27d209b4f9bc53a33f083f1e132f9d
Taoheed-O <oyeniyiemperor@gmail.com>
2026-08-16 21:18:29 +0100

    deployment..

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
